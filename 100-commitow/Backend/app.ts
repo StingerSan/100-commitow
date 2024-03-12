9f93d9ab312cd0843ecfe849055a640f6253b5e4
StingerSan <stinger@int.pl>
2024-03-12 21:38:12 +0100

    Work in progress

--- 100-commitow/Backend/app.ts
+++ 100-commitow/Backend/app.ts
@@ -1,12 +1,24 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
 
 const getWeatherData = async () => {
+  let city = "Katowice";
+  const key = process.env.KEY;
+
   try {
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.}`,
+    const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`,
     );
-    console.log(response);
+    const data = await res.json();
+    console.log(data);
   } catch (error) {
     console.log("there have been an", error);
   }
 };
 getWeatherData();
+
+app.listen(3000, "localhost", () => {
+  console.log("lisening on  http://localhost:3000");
+});
