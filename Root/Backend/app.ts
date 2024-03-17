dee608571d3748142279a43d3afffca91d193a7a
StingerSan <stinger@int.pl>
2024-03-17 22:07:25 +0100

    Work in progress

--- Root/Backend/app.ts
+++ Root/Backend/app.ts
@@ -3,21 +3,33 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const getWeatherData = async () => {
-  let city = "Katowice";
-  const key = process.env.KEY;
+// const getWeatherData = async () => {
+//   let city = "Katowice";
+//   const key = process.env.KEY;
+//
+//   try {
+//     const res = await fetch(
+//       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`,
+//     );
+//     const data = await res.json();
+//     console.log(data);
+//   } catch (error) {
+//     console.log("there have been an", error);
+//   }
+// };
+// getWeatherData();
 
-  try {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`,
-    );
-    const data = await res.json();
-    console.log(data);
-  } catch (error) {
-    console.log("there have been an", error);
-  }
-};
-getWeatherData();
+app.get("/", async (req, res) => {
+  const key = process.env.KEY;
+  let city = "Katowice";
+
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric`,
+  );
+  const weatherData = await response.json();
+  res.send(weatherData);
+  console.log(weatherData);
+});
 
 app.listen(3001, "localhost", () => {
   console.log("lisening on  http://localhost:3001");
--- Root/Frontend/src/components/SearchCity.tsx
+++ Root/Frontend/src/components/SearchCity.tsx
@@ -1,3 +1,16 @@
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 
-export const searchCity = () => {};
+export const SearchCity = () => {
+  const [search, setSearch] = useState("");
+
+  const handleCity = async (event: FormEvent) => {
+    event.preventDefault();
+
+    const cityName = await fetch("/", {
+      method: "POST",
+      headers: {
+        "content-Type": "application/json",
+      },
+    });
+  };
+};
